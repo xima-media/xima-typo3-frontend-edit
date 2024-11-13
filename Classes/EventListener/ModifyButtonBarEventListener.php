03f1bc8e434fa5b86849045374117c463998e3a0
Konrad Michalik <konrad.michalik@xima.de>
2024-11-13 11:01:13 +0100

    feat: add save and close button for edit form

---
+++ Classes/EventListener/ModifyButtonBarEventListener.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Xima\XimaTypo3FrontendEdit\EventListener;
+
+use TYPO3\CMS\Backend\Template\Components\ButtonBar;
+use TYPO3\CMS\Backend\Template\Components\Buttons\InputButton;
+use TYPO3\CMS\Backend\Template\Components\ModifyButtonBarEvent;
+use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+final class ModifyButtonBarEventListener
+{
+    public function __invoke(ModifyButtonBarEvent $event): void
+    {
+        $buttons = $event->getButtons();
+        $buttonBar = $event->getButtonBar();
+        $saveButton = $buttons[ButtonBar::BUTTON_POSITION_LEFT][2][0] ?? null;
+
+        if ($saveButton instanceof InputButton) {
+            $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
+            $saveCloseButton = $buttonBar->makeInputButton()
+                ->setName('_saveandclosedok')
+                ->setValue('1')
+                ->setForm($saveButton->getForm())
+                ->setTitle($this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:rm.saveCloseDoc'))
+                ->setIcon($iconFactory->getIcon('actions-document-save-close', Icon::SIZE_SMALL))
+                ->setShowLabelText(true);
+            $buttons[ButtonBar::BUTTON_POSITION_LEFT][2][] = $saveCloseButton;
+
+        }
+        $event->setButtons($buttons);
+    }
+
+    protected function getLanguageService(): LanguageService
+    {
+        return $GLOBALS['LANG'];
+    }
+}
