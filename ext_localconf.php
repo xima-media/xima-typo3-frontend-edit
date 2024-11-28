5bcf0e964b6807751fc57447ee5acb00f570eb78
Konrad Michalik <konrad.michalik@xima.de>
2024-11-28 16:59:04 +0100

    cleanup: several parts

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,3 @@
 <?php
 
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
-use Xima\XimaTypo3FrontendEdit\Configuration;
-use Xima\XimaTypo3FrontendEdit\Controller\EditController;
-
-ExtensionUtility::configurePlugin(
-    Configuration::EXT_NAME,
-    Configuration::PLUGIN_NAME,
-    [
-        EditController::class => 'contentElements',
-    ],
-    [
-        EditController::class => 'contentElements',
-    ]
-);
-
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['xtfe'] = ['Xima\\XimaTypo3FrontendEdit\\ViewHelpers'];
--- ext_tables.php
+++ ext_tables.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use Xima\XimaTypo3FrontendEdit\Configuration;
 
-defined('TYPO3') or die();
+(static function (): void {
+    $GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_ximatypo3frontendedit_disable'] = [
+        'label' => 'LLL:EXT:' . Configuration::EXT_KEY . '/Resources/Private/Language/locallang_db.xlf:be_users.tx_ximatypo3frontendedit_disable',
+        'description' => 'LLL:EXT:' . Configuration::EXT_KEY . '/Resources/Private/Language/locallang_db.xlf:be_users.tx_ximatypo3frontendedit_disable.description',
+        'type' => 'check',
+        'table' => 'be_users',
+    ];
 
-$GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_ximatypo3frontendedit_disable'] = [
-    'label' => 'LLL:EXT:' . Configuration::EXT_KEY . '/Resources/Private/Language/locallang_db.xlf:be_users.tx_ximatypo3frontendedit_disable',
-    'description' => 'LLL:EXT:' . Configuration::EXT_KEY . '/Resources/Private/Language/locallang_db.xlf:be_users.tx_ximatypo3frontendedit_disable.description',
-    'type' => 'check',
-    'table' => 'be_users',
-];
-
-ExtensionManagementUtility::addFieldsToUserSettings(
-    'tx_ximatypo3frontendedit_disable',
-    'after:copyLevels',
-);
+    ExtensionManagementUtility::addFieldsToUserSettings(
+        'tx_ximatypo3frontendedit_disable',
+        'after:copyLevels',
+    );
+})();
