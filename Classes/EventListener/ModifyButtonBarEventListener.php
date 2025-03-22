160e4af40519d2dc3f56b4f63413f8e01d03d677
Konrad Michalik <konrad.michalik@gmx.de>
2025-03-22 14:35:43 +0100

    fix: save and close functionality for v13

--- Classes/EventListener/ModifyButtonBarEventListener.php
+++ Classes/EventListener/ModifyButtonBarEventListener.php
@@ -8,7 +8,9 @@ use TYPO3\CMS\Backend\Template\Components\ModifyButtonBarEvent;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Page\PageRenderer;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use Xima\XimaTypo3FrontendEdit\Configuration;
 
@@ -44,6 +46,18 @@ final class ModifyButtonBarEventListener
                 ->setTitle($this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:rm.saveCloseDoc'))
                 ->setIcon($iconFactory->getIcon('actions-document-save-close', Icon::SIZE_SMALL))
                 ->setShowLabelText(true);
+
+            $typo3Version = GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion();
+            if ($typo3Version >= 13) {
+                $saveCloseButton->setDataAttributes([
+                    'js' => 'save-close',
+                ]);
+
+                /** @var PageRenderer $pageRenderer */
+                $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
+                $pageRenderer->loadJavaScriptModule('@xima/ximatypo3frontendedit/save_close.js');
+            }
+
             $buttons[ButtonBar::BUTTON_POSITION_LEFT][2][] = $saveCloseButton;
         }
         $event->setButtons($buttons);
