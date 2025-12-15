7b5cfcaa59efa717c281c4798598af63d317e8a1
Konrad Michalik <km@move-elevator.de>
2025-12-15 14:52:19 +0100

    feat: add support for PHP 8.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.7.2',
     'constraints' => [
         'depends' => [
-            'php' => '8.2.0-8.4.99',
+            'php' => '8.2.0-8.5.99',
             'typo3' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
